3c07c50ba80b32178085d8c54969ecaa5b799186
Mohammed Danish <m4mohd.dany03@gmail.com>
2021-06-05 22:14:41 -0700

    design commit

--- project_1/lib/BookTable.dart
+++ project_1/lib/BookTable.dart
@@ -7,8 +7,6 @@ class Booking extends StatefulWidget {
 }
 
 class _BookingState extends State<Booking> {
-  //choice chips
-  int? _value = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,20 +28,20 @@ class _BookingState extends State<Booking> {
         child: Container(
           child: Column(
             children: [
-              SizedBox(height: 10),
+              SizedBox(height: 15),
               Align(
                   alignment: Alignment.center,
                   child: Text(
                     'Table Reservation',
                     style: TextStyle(
-                        fontSize: 20,
+                        fontSize: 21,
                         color: Color(0xff442c2e),
                         fontWeight: FontWeight.bold),
                   )),
               SizedBox(height: 10),
               //date card
               Card(
-                color: Color(0xfffedbd0),
+                color: Color(0xFFFCFBFA),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -58,62 +56,302 @@ class _BookingState extends State<Booking> {
                       ),
                     ),
                     Container(
-                      padding: EdgeInsets.all(8),
+                      padding: EdgeInsets.all(10),
                       child: DatePicker(DateTime.now(),
-                          width: 60,
-                          height: 80,
+                          width: 65,
+                          height: 100,
                           initialSelectedDate: DateTime.now(),
-                          selectionColor: Color(0xff442c2e),
-                          selectedTextColor: Colors.white,
+                          selectionColor: Color(0xfffedbd0),
+                          selectedTextColor: Color(0xff442c2e),
                           onDateChange: (date) {
                         // New date selected
                         setState(() {});
                       }),
-                    )
+                    ),
+                    SizedBox(height: 10)
                   ],
                 ),
               ),
+
               //Time card
               Card(
-                color: Color(0xfffedbd0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20.0, top: 20),
-                      child: Text(
-                        'Date',
-                        style: TextStyle(
-                            fontSize: 18,
-                            color: Color(0xff442c2e),
-                            fontWeight: FontWeight.bold),
+                color: Color(0xFFFDFBFB),
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  height: 170,
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 10.0, top: 10, bottom: 10),
+                        child: Text(
+                          'Time',
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Color(0xff442c2e),
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
-                    ),
-                    Container(
-                        padding: EdgeInsets.all(8),
-                        child: Row(
-                          children: List<Widget>.generate(
-                            5,
-                            (int index) {
-                              return ChoiceChip(
-                                label: Text('Item $index'),
-                                selected: _value == index,
-                                onSelected: (bool selected) {
-                                  setState(() {
-                                    _value = selected ? index : null;
-                                  });
-                                },
-                              );
-                            },
-                          ).toList(),
-                        ))
-                  ],
+                      timeChips(context)
+                    ],
+                  ),
                 ),
               ),
+              guest(context),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  'Book Now',
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Color(0xff442c2e),
+                      fontWeight: FontWeight.bold),
+                ),
+                style: ButtonStyle(
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                        EdgeInsets.only(
+                            left: 120, right: 120, top: 15, bottom: 15)),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Color(0xFFFEDBD0)),
+                    shape: MaterialStateProperty.all<StadiumBorder>(
+                        StadiumBorder())),
+              )
             ],
           ),
         ),
       ),
     );
   }
+}
+
+//time Chips
+Widget timeChips(BuildContext context) {
+  return Column(
+    children: [
+      Row(
+        children: [
+          Expanded(
+            child: ActionChip(
+                label: const Text(
+                  '2:00',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xff442c2e),
+                      fontWeight: FontWeight.bold),
+                ),
+                backgroundColor: Color(0xFFFAF5F4),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Color(0xff442c2e), width: 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                onPressed: () {}),
+          ),
+          Expanded(
+            child: ActionChip(
+                label: const Text(
+                  '2:30',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xff442c2e),
+                      fontWeight: FontWeight.bold),
+                ),
+                backgroundColor: Color(0xFFFAF5F4),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Color(0xff442c2e), width: 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                onPressed: () {}),
+          ),
+          Expanded(
+            child: ActionChip(
+                label: const Text(
+                  '3:00',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xff442c2e),
+                      fontWeight: FontWeight.bold),
+                ),
+                backgroundColor: Color(0xFFFAF5F4),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Color(0xff442c2e), width: 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                onPressed: () {}),
+          ),
+          Expanded(
+            child: ActionChip(
+                label: const Text(
+                  '4:00',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xff442c2e),
+                      fontWeight: FontWeight.bold),
+                ),
+                backgroundColor: Color(0xFFFAF5F4),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Color(0xff442c2e), width: 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                onPressed: () {}),
+          ),
+          Expanded(
+            child: ActionChip(
+                label: const Text(
+                  '4:30',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xff442c2e),
+                      fontWeight: FontWeight.bold),
+                ),
+                backgroundColor: Color(0xFFFAF5F4),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Color(0xff442c2e), width: 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                onPressed: () {}),
+          ),
+        ],
+      ),
+      Row(
+        children: [
+          Expanded(
+            child: ActionChip(
+                label: const Text(
+                  '5:00',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xff442c2e),
+                      fontWeight: FontWeight.bold),
+                ),
+                backgroundColor: Color(0xFFFAF5F4),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Color(0xff442c2e), width: 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                onPressed: () {}),
+          ),
+          Expanded(
+            child: ActionChip(
+                label: const Text(
+                  '6:00',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xff442c2e),
+                      fontWeight: FontWeight.bold),
+                ),
+                backgroundColor: Color(0xFFFAF5F4),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Color(0xff442c2e), width: 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                onPressed: () {}),
+          ),
+          Expanded(
+            child: ActionChip(
+                label: const Text(
+                  '6:30',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xff442c2e),
+                      fontWeight: FontWeight.bold),
+                ),
+                backgroundColor: Color(0xFFFAF5F4),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Color(0xff442c2e), width: 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                onPressed: () {}),
+          ),
+          Expanded(
+            child: ActionChip(
+                label: const Text(
+                  '7:00',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xff442c2e),
+                      fontWeight: FontWeight.bold),
+                ),
+                backgroundColor: Color(0xFFFAF5F4),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Color(0xff442c2e), width: 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                onPressed: () {}),
+          ),
+          Expanded(
+            child: ActionChip(
+                label: const Text(
+                  '8:00',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Color(0xff442c2e),
+                      fontWeight: FontWeight.bold),
+                ),
+                backgroundColor: Color(0xFFFAF5F4),
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Color(0xff442c2e), width: 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                onPressed: () {}),
+          ),
+        ],
+      ),
+    ],
+  );
+}
+
+//Guest
+Widget guest(BuildContext context) {
+  return Card(
+    color: Color(0xFFFDFBFB),
+    child: Container(
+      padding: EdgeInsets.all(10),
+      height: 80,
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        children: [
+          SizedBox(width: 20),
+          Expanded(
+            child: Text(
+              'No of People',
+              style: TextStyle(
+                  fontSize: 18,
+                  color: Color(0xff442c2e),
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(width: 50),
+          Expanded(
+            child: Container(
+              color: Color(0xFFFEdBD0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+                  Container(
+                    height: 40,
+                    width: 40,
+                    color: Colors.white,
+                    child: Center(
+                      child: Text(
+                        '6',
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0xff442c2e),
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.remove))
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    ),
+  );
 }
